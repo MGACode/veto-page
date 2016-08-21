c567826e8386366bcfd7b0d73c0a01ea2cc8397f
AriaFallah <aria@fallah.us>
2016-08-21 14:50:25 -0400

    :new: working websockets

--- app/src/components/MapPicker/index.js
+++ app/src/components/MapPicker/index.js
@@ -1,110 +1,137 @@
 // @flow
 
-import React, { Component } from 'react'
 import { partial } from 'lodash'
 import io from 'socket.io-client'
 import { remove } from 'lodash/fp'
 import { observer } from 'mobx-react'
-import { action, autorun, computed, observable, toJS } from 'mobx'
-import c from './styles'
+import React, { Component } from 'react'
+import { action, autorun, computed, observable } from 'mobx'
+import styles from './styles'
 
 // Websocket connection
 const socket = io('localhost:3000')
+
+// Data
 const COUNTER_MAX = 5
 const statusTypes = ['Ban', 'Ban', 'Pick', 'Pick']
 const mapPool = [
-  { name: 'Hideout', image: 'http://i.imgur.com/RwEDe4g.png', team: '', type: '' },
-  { name: 'Battlegrounds', image: 'http://i.imgur.com/RKse50K.png', team: '', type: '' },
-  { name: 'Cove', image: 'http://i.imgur.com/hZJpmzJ.png', team: '', type: '' },
-  { name: 'Hillside', image: 'http://i.imgur.com/lkZTlsz.png', team: '', type: '' },
-  { name: 'Outpost', image: 'http://i.imgur.com/9g4ulbt.png', team: '', type: '' },
-  { name: 'Stoneshill', image: 'http://i.imgur.com/eVTjMom.png', team: '', type: '' },
-  { name: 'Darkforest', image: 'http://i.imgur.com/NBZyfBa.png', team: '', type: '' }
+  { name: 'Hideout', image: 'http://i.imgur.com/RwEDe4g.png', team: 0, type: '' },
+  { name: 'Battlegrounds', image: 'http://i.imgur.com/RKse50K.png', team: 0, type: '' },
+  { name: 'Cove', image: 'http://i.imgur.com/hZJpmzJ.png', team: 0, type: '' },
+  { name: 'Hillside', image: 'http://i.imgur.com/lkZTlsz.png', team: 0, type: '' },
+  { name: 'Outpost', image: 'http://i.imgur.com/9g4ulbt.png', team: 0, type: '' },
+  { name: 'Stoneshill', image: 'http://i.imgur.com/eVTjMom.png', team: 0, type: '' },
+  { name: 'Darkforest', image: 'http://i.imgur.com/NBZyfBa.png', team: 0, type: '' }
 ]
-
-const state = {
-  counter: 0,
-  maps: mapPool,
-  team1: 'Team 1',
-  team2: 'Team 2',
-  remaining: [...Array(mapPool.length).keys()]
-}
 
 @observer
 export default class MapPicker extends Component {
   constructor(props) {
     super(props)
-    autorun(() => socket.emit('data', toJS(this.status)))
+
+    autorun(() => {
+      if (this.changed) {
+        this.changed = false
+        return
+      }
+      socket.emit('data', {
+        maps: this.maps,
+        team1: this.team1,
+        team2: this.team2,
+        counter: this.counter,
+        remaining: this.remaining
+      })
+    })
+
+    socket.on('data', action('Receive changes', (data) => {
+      this.changed = true
+      this.maps = data.maps
+      this.team1 = data.team1
+      this.team2 = data.team2
+      this.counter = data.counter
+      this.remaining = data.remaining
+    }))
   }
 
-  @observable status = state
+  changed = true;
+  @observable counter = 0;
+  @observable maps = mapPool;
+  @observable team1 = 'Team 1';
+  @observable team2 = 'Team 2';
+  @observable remaining = [...Array(mapPool.length).keys()];
 
   @action update = (team, event) => {
-    this.status[team] = event.target.value
+    this[team] = event.target.value
   }
 
   @action select = (index) => {
-    const map = this.status.maps[index]
+    const map = this.maps[index]
 
     if (map.team) return
-    if (this.status.counter > COUNTER_MAX) return
+    if (this.counter > COUNTER_MAX) return
 
-    const subset = remove((x) => x === index)(this.status.remaining)
+    const subset = remove((x) => x === index)(this.remaining)
 
-    this.status.maps[index].type = statusTypes[this.status.counter % 4]
-    this.status.maps[index].team = this.status.counter % 2 === 0
-      ? computed(() => this.status.team1)
-      : computed(() => this.status.team2)
+    this.maps[index].type = statusTypes[this.counter % 4]
+    this.maps[index].team = this.counter % 2 === 0 ? 1 : 2
 
     if (subset.length === 1) {
       const idx = subset[0]
-      this.status.maps[idx].type = 'Random'
-      this.status.maps[idx].team = computed(() => 'Random')
+      this.maps[idx].team = 3
+      this.maps[idx].type = 'Random'
     }
 
-    this.status.remaining = subset
-    this.status.counter = this.status.counter + 1
+    this.remaining = subset
+    this.counter = this.counter + 1
   }
+
+  team = (team) => computed(() => {
+    switch (team) {
+    case 0: return ''
+    case 1: return this.team1
+    case 2: return this.team2
+    case 3: return 'Random'
+    default: return 'default'
+    }
+  })
 
   teamClass = (team) => {
     switch (team) {
-    case '': return 'default'
-    case this.status.team1: return 'team-1'
-    case this.status.team2: return 'team-2'
+    case 0: return 'default'
+    case 1: return 'team-1'
+    case 2: return 'team-2'
+    case 3: return 'random'
     default: return 'default'
     }
   }
 
   render() {
+    const updateTeam1 = action('Change "Team 1" name', partial(this.update, 'team1'))
+    const updateTeam2 = action('Change "Team 2" name', partial(this.update, 'team2'))
+
     return (
       <div>
         <div className="ui three column centered grid">
           <div className="ui inverted horizontal divider">Enter Team Names</div>
           <div className="ui column input">
-            <input
-              value={this.status.team1}
-              onChange={action('Change "Team 1" name', partial(this.update, 'team1'))}
-            />
+            <input value={this.team1} onChange={updateTeam1} />
           </div>
           <div className="column"></div>
           <div className="ui column input">
-            <input
-              value={this.status.team2}
-              onChange={action('Change "Team 2" name', partial(this.update, 'team2'))}
-            />
+            <input value={this.team2} onChange={updateTeam2} />
           </div>
         </div>
         <div className="ui inverted horizontal divider">Pick Maps</div>
         <div className="ui centered three column grid">
-          {this.status.maps.map((map, index) =>
+          {this.maps.map((map, index) =>
             <div className="column" key={map.name} onClick={action('select', partial(this.select, index))}>
               <div
                 style={{ backgroundImage: `url(${map.image})` }}
-                className={`${c.mapPanel} ${this.teamClass(map.team && map.team.get())}`}
+                className={`${styles.mapPanel} ${this.teamClass(map.team)}`}
               >
-                <div className={c.mapName}>{map.name}</div>
-                <div>{map.team && map.team.get()}</div>
-                <div className={`${c[map.type.toLowerCase()]} ${c.status}`}>{map.type}</div>
+                <div className={styles.mapName}>{map.name}</div>
+                <div>{this.team(map.team).get()}</div>
+                <div className={`${styles[map.type.toLowerCase()]} ${styles.status}`}>{map.type}</div>
               </div>
             </div>)
           }
